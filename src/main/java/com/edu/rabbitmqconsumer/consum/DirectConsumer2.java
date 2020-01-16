14b5470ea0e4e6e99c70e6fa010d13e59bac4a0c
wangcunlei <wangcunlei@zhizhangyi.com>
2020-01-16 18:31:05 +0800

    first commit

---
+++ src/main/java/com/edu/rabbitmqconsumer/consum/DirectConsumer2.java
@@ -0,0 +1,57 @@
+package com.edu.rabbitmqconsumer.consum;
+
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RabbitListener(queues = "TestDirectQueue")
+public class DirectConsumer2 implements ChannelAwareMessageListener {
+
+    /*@RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }*/
+
+    @Override
+    public void onMessage(Message message, Channel channel) throws Exception {
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        try {
+            //因为传递消息的时候用的map传递,所以将Map从Message内取出需要做些处理
+            long startTime = System.currentTimeMillis();
+            String msg = message.toString();
+            String[] msgArray = msg.split("'");//可以点进Message里面看源码,单引号直接的数据就是我们的map消息数据
+            Map<String, String> msgMap = mapStringToMap(msgArray[1].trim());
+            String messageId=msgMap.get("messageId");
+            String messageData=msgMap.get("messageData");
+            String createTime=msgMap.get("createTime");
+            System.out.println("messageId:"+messageId+"  messageData:"+messageData+"  createTime:"+createTime);
+            channel.basicAck(deliveryTag, true);
+            long endTime = System.currentTimeMillis();
+            System.out.println("conumer2 cost time -->>"+(endTime-startTime));
+//			channel.basicReject(deliveryTag, true);//为true会重新放回队列
+        } catch (Exception e) {
+            channel.basicReject(deliveryTag, false);
+            e.printStackTrace();
+        }
+
+    }
+
+    private Map<String, String> mapStringToMap(String str) {
+        str = str.substring(1, str.length() - 1);
+        String[] strs = str.split(",");
+        Map<String, String> map = new HashMap<String, String>();
+        for (String string : strs) {
+            String key = string.split("=")[0].trim();
+            String value = string.split("=")[1];
+            map.put(key, value);
+        }
+        return map;
+    }
+
+}
